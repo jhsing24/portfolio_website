c0f4b7932459d772f2d8353427be2499b5711346
jhsing24 <jerohsing@gmail.com>
2022-06-20 11:39:27 +0900

    First upload up to "about" section

---
+++ frontend_react/src/container/About/About.jsx
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react'
+import {motion} from 'framer-motion'
+import {images} from '../../constants'
+
+import './About.scss'
+ 
+const abouts = [
+  {title: 'Frontend Development', description: 'I have had experience creating responsive websites using various frameworks and software.', imgUrl: images.about01},
+  {title: 'Web Design', description: 'I have an eye for design and can create assets that help accentuate a website.', imgUrl: images.about02},
+  {title: 'UI/UX', description: 'I pay close attention to usability and accessibility of websites, and I always aim to improve user experience.', imgUrl: images.about03},
+
+
+]
+const About = () => {
+  return (
+    <>
+    <h2 className='head-text'>I know that
+      <span> Good Design </span>
+      means
+      <span> Good Business </span>
+    </h2>
+
+    <div className='app__profiles'>
+      {abouts.map((about, index) => (
+        <motion.div 
+        whileInView={{opacity: 1}}
+        whileHover={{scale: 1.1}}
+        transition={{duration: 0.5, type: 'tween'}}
+        className='app__profile-item'
+        key={about.title + index}>
+          <img src={about.imgUrl} alt='about.title'/>
+          <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
+          <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
+        </motion.div>
+      ))}
+    </div>
+    </>
+  )
+}
+
+export default About
